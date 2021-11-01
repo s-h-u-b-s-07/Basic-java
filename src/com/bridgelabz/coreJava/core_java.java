9cb9c122ed38df77e0750fcc54d7395df0dc76fc
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-01 13:04:55 +0530

    leap year detection

--- src/com/bridgelabz/coreJava/core_java.java
+++ src/com/bridgelabz/coreJava/core_java.java
@@ -4,38 +4,14 @@ import java.util.Scanner;
 
 public class core_java {
     public static void main(String[] args) {
+        int year;
+        System.out.println("Enter an Year :: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of times coin flip");
-        int totalFlip = sc.nextInt();
+        year = sc.nextInt();
 
-
-        //variables
-        int count = 0;
-        int head = 0;
-        int tail = 0;
-
-        //computation
-        while (count != totalFlip) {
-            double flip = Math.random();
-            System.out.println(flip);
-
-            if (flip < 0.5) {
-                System.out.println("Head");
-                head++;
-
-            } else {
-                System.out.println("Tail");
-                tail++;
-            }
-            count++;
-        }
-
-        System.out.println("number of heads wins: " + head);
-        System.out.println("number of tails wins:" + tail);
-
-        int perHaid = (head * 100 / totalFlip);
-        int perTail = (tail * 100 / totalFlip);
-        System.out.println("the percentage of head win:" + perHaid);
-        System.out.println("the percentage of tail win:" + perTail);
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            System.out.println("Specified year is a leap year");
+        else
+            System.out.println("Specified year is not a leap year");
     }
 }
